d0fdd483c83fad8efab3ada8ef1af21573054bd0
Jakub Antczak <jakubantczak.info@gmail.com>
2019-06-14 11:59:43 +0200

    SideDrawer and responsive Adjustment!

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -3,10 +3,7 @@ import classes from './NavigationItem.css';
 
 const navigationItem = (props) => (
     <li className={classes.NavigationItem}>
-        <a 
-        href={props.link} 
-        className={props.active ? classes.active : null}
-        >
+        <a href={props.link} className={props.active ? classes.active : null}>
             {props.children}
         </a>
     </li>
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -6,8 +6,12 @@ import NavigationItems from '../NavigationItems/NavigationItems';
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
         <div>MENU</div>
+        <div className={classes.Logo}>
         <Logo />
-        <NavigationItems />
+        </div>
+        <nav className={classes.DesktopOnly}>
+            <NavigationItems />
+        </nav>
     </header>
 );
 
