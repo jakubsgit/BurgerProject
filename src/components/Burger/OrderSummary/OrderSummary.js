bfe5de24a2d03ceec17a011ce4231000ed25d8f4
Jakub Antczak <jakubantczak.info@gmail.com>
2019-06-14 13:41:23 +0200

    Creating Lifecycle methods

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,30 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button'
 
-const orderSummary = (props) => {
-    
-    const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+        componentDidUpdate() {
+            console.log('[OrderSummary] DidUpdate');
+        }
+
+        render () {
+
+        const ingredientSummary = Object.keys(this.props.ingredients)
         .map(igKey => {
             return (
             <li key={igKey}>
-                <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
+                <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
             </li>);
-        })
-    
-    return (
-        <Aux>
+        })        
+
+        return (
+            <Aux>
             <h3>Your Order</h3>
             <p>A delicious Burger with the following ingredients:</p>
             <ul>
                 {ingredientSummary}
             </ul>
-            <p><strong>Total Price: </strong>{props.price.toFixed(2)}</p>
+            <p><strong>Total Price: </strong>{this.props.price.toFixed(2)}</p>
             <p>Continue to Checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseCanceled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
+            <Button btnType="Danger" clicked={this.props.purchaseCanceled}>CANCEL</Button>
+            <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
         </Aux>
-    )
+        )
+    }
 }
 
-export default orderSummary;
+export default OrderSummary;
--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import menuLogo from '../../../../assets/images/menu-logo.png';
+import classes from './DrawerToggle.css'
 
 const drawerToggle = (props) => (
-    <div onClick={props.clicked}>MENU</div>
+    <div onClick={props.clicked}>
+        <img src={menuLogo} alt="mobile-menu-logo" className={classes.MobileLogo}/>
+    </div>
 );
 
 export default drawerToggle;
