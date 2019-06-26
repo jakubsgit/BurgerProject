47a46ec68dd3892a956a2cefe3d9f04e1edc5174
Jakub Antczak <jakubantczak.info@gmail.com>
2019-06-26 11:36:27 +0200

    axios added

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -27,13 +27,17 @@ class BurgerBuilder extends Component {
         purchasable: false,
         purchasing: false,
         loading: false,
+        error: false
     }
 
     componentDidMount() {
       axios.get('https://burgerburger-4f1b9.firebaseio.com/ingredients.json')
         .then(response => {
-          this.setState({ingredients: response.data});
-        });
+          this.setState({ingredients: response.data})
+        })
+          .catch(error => {
+            this.setState({error: true})
+          });
     }
     
 
