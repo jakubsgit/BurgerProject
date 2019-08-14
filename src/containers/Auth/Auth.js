82442c8d42e9e98d22254180869ac9529a85473a
Jakub Antczak <jakubantczak.info@gmail.com>
2019-08-14 11:00:26 +0200

    redirect do panelu logowania, jezeli mamy token

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
@@ -119,9 +120,14 @@ class Auth extends Component {
                 <p>{this.props.error.message}</p>
             );
         }
-        
+
+        let authRedirect = null;
+        if (this.props.isAuthenticated) {
+            authRedirect = <Redirect to="/"/>
+        };
         return (
             <div className={classes.Auth}>
+                {authRedirect}
                 <h1>{this.state.isSignUp? 'Signing Up' : 'Signing In'}</h1>
                 {errorMessage}
                 <form onSubmit={this.onSubmitHandler}>
@@ -139,7 +145,8 @@ class Auth extends Component {
 const mapStateToProps = state => {
     return {
         loading: state.auth.loading,
-        error: state.auth.error
+        error: state.auth.error,
+        isAuthenticated: state.auth.token !== null
     }
 }
 
