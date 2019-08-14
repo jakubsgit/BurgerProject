6481eb1b8fb3bb15dee104c558370bcd61f8c36e
Jakub Antczak <jakubantczak.info@gmail.com>
2019-08-14 10:46:07 +0200

    Nawigowanie do Logout jezeli mamy token i zmiana UI

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -23,7 +23,7 @@ const sideDrawer = (props) => {
                     <Logo />
                 </div>
                 <nav>
-                    <NavigationItems />
+                    <NavigationItems isAuthenticated={props.isAuth}/>
                 </nav>
             </div>
         </Aux>
